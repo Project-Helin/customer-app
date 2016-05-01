3a59fc558b729810054f5c14775a88290f8ded76
Marcel Amsler <marcel.amsler@gmail.com>
2016-05-01 11:10:12 +0200

    added first page with navigation-stack

---
+++ ProductListPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace customerapp
+{
+    public partial class ProductListPage : ContentPage
+    {
+        public ProductListPage () 
+        {
+            InitializeComponent();
+        }
+
+        void OnShowOrdersClick(object sender, EventArgs args) {
+        }
+
+        void OnShowProductButtonClick(object sender, EventArgs args) {
+        }
+
+
+ 
+    }
+}
+
