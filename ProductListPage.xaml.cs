4968ba63732fe2008c713e4faf47193fd4d69427
Kirusanth Poopalasingam <pkirusanth@gmail.com>
2016-05-18 15:44:56 +0200

    disabled paypal integration

--- ProductListPage.xaml.cs
+++ ProductListPage.xaml.cs
@@ -11,7 +11,7 @@ using PayPal.Forms.Abstractions;
 using PayPal.Forms.Abstractions.Enum;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Collections.Generic;
+
 
 namespace customerapp
 {
@@ -20,43 +20,27 @@ namespace customerapp
         ObservableCollection<Product> products = new ObservableCollection<Product>();
         ObservableCollection<Product> orderedProducts = new ObservableCollection<Product>();
 
+		RestService restService = new RestService();
+
         public ProductListPage()
         {
             InitializeComponent();
+			ProductListView.ItemsSource = products;
+			ProductListView.ItemTapped += ProductTapped;
         }
 
 		protected async override void OnAppearing(){
 			base.OnAppearing ();
-			var products2 = await FetchProduct();
-			foreach (Product b in products2) {
-				products.Add (b);
+
+			products.Clear ();
+
+			var productsFromServer = await restService.GetAllProducts();
+			foreach (Product each in productsFromServer) {
+				products.Add (each);
 			}
-				
-			ProductListView.ItemsSource = products;
-			ProductListView.ItemTapped += ProductTapped;
+
 		}
 
-		public async Task<List<Product>> FetchProduct()
-		{
-			var client = new HttpClient ();
-			using (client) {
-
-				Debug.WriteLine ("Fetch order ");
-				// TODO we need a common place for this
-				var uri = new Uri ("http://192.168.222.1:9000/api/products/");
-				var response = await client.GetAsync (uri);
-				List<Product> items = new List<Product>();
-
-				if (response.IsSuccessStatusCode) {
-					var content = await response.Content.ReadAsStringAsync ();
-					items = Newtonsoft.Json.JsonConvert.DeserializeObject <List<Product>> (content);
-				} else {
-					Debug.WriteLine ("Failed to get all order with status code " + response.StatusCode);
-				}
-			
-				return items;
-			}
-		}
 
 
 		void ProductTapped(object sender, ItemTappedEventArgs e)
@@ -74,25 +58,38 @@ namespace customerapp
         async void OnOrderButtonClick(object sender, EventArgs args)
         {
             var orderSum = orderedProducts.Sum((a) => a.Amount * a.Price);
-            var orderConfirmed = await DisplayAlert("Produkte Kaufen?", "Möchten Sie die Bestellung im Wert von" + orderSum + " CHF abschicken?", "Ja", "Nein");
+            var orderConfirmed = await DisplayAlert("Produkte Kaufen?", "Möchten Sie die Bestellung im Wert von " + orderSum + " CHF abschicken?", "Ja", "Nein");
 
             if (orderConfirmed)
             {
         
-                var result = await CrossPayPalManager.Current.Buy(new PayPalItem("Order 123", (Decimal) orderSum, "CHF"), new Decimal(0));
-                if (result.Status == PayPalStatus.Cancelled)
-                {
-                    Debug.WriteLine("Cancelled");
-                }
-                else if (result.Status == PayPalStatus.Error)
-                {
-                    Debug.WriteLine(result.ErrorMessage);
-                }
-                else if (result.Status == PayPalStatus.Successful)
-                {
-                    Debug.WriteLine("Payment Successful");
-                    Debug.WriteLine(result.ServerResponse.Response.Id);
-                }
+				// let user pay the order ... 
+//                var result = await CrossPayPalManager.Current.Buy(new PayPalItem("Order 123", (Decimal) orderSum, "CHF"), new Decimal(0));
+//                if (result.Status == PayPalStatus.Cancelled)
+//                {
+//                    Debug.WriteLine("Cancelled");
+//                }
+//                else if (result.Status == PayPalStatus.Error)
+//                {
+//                    Debug.WriteLine(result.ErrorMessage);
+//                }
+//                else if (result.Status == PayPalStatus.Successful)
+//                {
+//                    Debug.WriteLine("Payment Successful");
+//                    Debug.WriteLine(result.ServerResponse.Response.Id);
+//                }
+
+				// send the order
+				Debug.WriteLine("Payment Successful");
+				// get Location 
+
+				// send order
+				var response = await restService.CreateOrder(orderedProducts);
+				var deliveryPosition = response.DeliveryPosition;
+				Debug.WriteLine (deliveryPosition); 
+
+				await Navigation.PushAsync(new OrderConfirmPage(response));
+
             }
           
         }
