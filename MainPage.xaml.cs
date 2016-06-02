b978cc39e1bbfb960dc0fb2f4aa035ab75df8e6f
Kirusanth Poopalasingam <pkirusanth@gmail.com>
2016-06-02 20:30:37 +0200

    refactored MainPage

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,6 +5,7 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Xamarin.Auth;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace customerapp
 {
@@ -19,18 +20,20 @@ namespace customerapp
 		protected async override void OnAppearing(){
 			base.OnAppearing ();
 
-			var customerId = Settings.GetCustomerIdOrNull ();
-			var loadCustomer =  !string.IsNullOrWhiteSpace(customerId) && App.Customer == null;
-			if (loadCustomer) {
-				App.Customer = await App.Rest.GetCustomerById (customerId);	
-
-			} else {
-				Debug.WriteLine ("Not loaded customer because hasCustomer={0} and isCustomerNull = {1}", 
-					loadCustomer, App.Customer == null);
-			}
-
+            await loadCustomerIfAvailable ();
 		}
 
+        private async Task loadCustomerIfAvailable(){
+            var customerId = Settings.GetCustomerIdOrNull ();
+
+            var noCustomerAvailable =  !string.IsNullOrWhiteSpace(customerId) && App.Customer == null;
+            if (noCustomerAvailable) {
+                App.Customer = await App.Rest.GetCustomerById (customerId); 
+            } else {
+                Debug.WriteLine ("No customer loaded because hasCustomer={0} and isCustomerNull = {1}", 
+                    noCustomerAvailable, App.Customer == null);
+            }  
+        }
 
         async void OnShowOrdersClick(object sender, EventArgs args) {
             await Navigation.PushAsync(new OrderListPage());
